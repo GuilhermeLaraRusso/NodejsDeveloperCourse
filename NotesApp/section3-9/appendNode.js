725e6503ddf7ea2309c79da291a700cbb16cdcea
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2022-01-14 10:19:31 -0300

    starting module system

---
+++ NotesApp/section3-9/appendNode.js
@@ -0,0 +1,10 @@
+/** Challange: Append a message to notes.txt 
+ * 
+ * 1. Use appendFileSync to append to the file
+ * 2. Run the script
+ * 3. Check your work by openning the file and viewing the appended text
+*/
+
+const fs = require('fs');
+
+fs.appendFileSync('notes.txt', '\nThis is the text added!');
