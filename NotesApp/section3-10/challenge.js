410de3f92ca54519b9cfbd5bcd8b843a986f01c5
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2022-01-14 10:33:32 -0300

    section 3-10

---
+++ NotesApp/section3-10/challenge.js
@@ -0,0 +1,7 @@
+/** Challenge: Define and use a fucntion in a new file
+ * 
+ * 1. Create a new file called notes.js
+ * 2. Create getNotes function that returns "Your notes..."
+ * 3. Export getNotes function
+ * 4. Freom app.js, load in and call the function printing message to console
+ */
